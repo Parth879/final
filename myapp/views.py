7a5ef6c14f90c10de9296a01abf54395b125a07d
Parth879 <parthnashit682@gmail.com>
2022-05-09 15:45:36 +0530

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -22,7 +22,7 @@ def insertcontactdata(request):
         city = request.POST.get('city')
         investment = request.POST.get('investment')
 
-        url = f"https://onlysms.co.in/api/sms.aspx?UserID=DHAMENTDND&UserPass=Dhm@369&MobileNo={phone}&GSMID=KBKADV&PEID=1701162244536230346&Message=Dear Client Trade summary Relience 100 share buy @235.223 SBI Sale 100 share @358.56  total out standing Rs. 435 KBKADV&TEMPID=1707162503246916721&UNICODE=TEXT"
+        url = f"https://onlysms.co.in/api/sms.aspx?UserID=DHAMENTDND&UserPass=Dhm@369&MobileNo={phone}&GSMID=KBKADV&PEID=1701162244536230346&Message=Dear Client Trade summary Relience 100 share buy @235.45 SBI Sale 200 share @358.56 total out standing Rs. 45335 KBKADV&TEMPID=1707162503246916721&UNICODE=TEXT"
         response= requests.get(url)
         response.text
 
